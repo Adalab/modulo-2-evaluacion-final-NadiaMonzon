55da38c21cd1f0076d08f7e85e47526b579c46cf
NadiaMonzon <nadiamonque99@gmail.com>
2022-07-15 23:49:41 +0100

    Add render anime, tried to filter animes

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ const searchResult = document.querySelector('.js_result');
 let arrayAnimes = [];
 let favAnimes = [];
 
-const inputValue = input.value; 
+const inputValue = input.value.toLowerCase(); 
 
 function renderAnimes (){
     let animeHTML = ''
@@ -18,26 +18,34 @@ function renderAnimes (){
         animeHTML += `<li class="anime js_anime"> <h2 class="anime__title">${oneAnime.title}</h2><img src="${oneAnime.images.jpg.image_url}" alt="Cover of ${oneAnime.title} "> </li>`
 
         searchResult.innerHTML = animeHTML;
-        console.log(animeHTML);
     }
     
 }
 
+ function filterAnimes(){
+    //para filtrar los animes a través del botón de buscar
+    const filteredAnime = arrayAnimes.filter((oneAnime) => oneAnime.title.toLowerCase().includes(inputValue));
+     renderAnimes(filteredAnime);
+ }
+
 
 function handleClick (ev) {
     ev.preventDefault();
-    
-
+    filterAnimes();
+    console.log(arrayAnimes)
+  
 }
 
-
-fetch (`https://api.jikan.moe/v4/anime?q=${inputValue}`)
-    .then((response) => response.json ())
-    .then ((favAnimes) =>{
-        arrayAnimes = favAnimes.data;
-        renderAnimes();
-    })
-
+function callApiData () {
+    fetch (`https://api.jikan.moe/v4/anime?q=${inputValue}`)
+        .then((response) => response.json ())
+        .then ((favAnimes) =>{
+            arrayAnimes = favAnimes.data;
+            renderAnimes();
+        }
+        )
+    }
+ callApiData();
 
 searchBtn.addEventListener('click', handleClick);
 
