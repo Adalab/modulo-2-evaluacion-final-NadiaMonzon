ed40ccd90991bb913af0a346c4019238fafa01a3
NadiaMonzon <nadiamonque99@gmail.com>
2022-07-18 12:37:11 +0100

    add filters, refactor other functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,51 @@
 'use strict';
 
-const input = document.querySelector('.js_input');
+const inputFilter = document.querySelector('.js_input');
 const searchBtn = document.querySelector('.js_search-btn');
 const resetBtn = document.querySelector('.js_reset-btn');
 
 const searchResult = document.querySelector('.js_result');
+const favList = document.querySelector('.js_fav')
 
-const inputValue = input.value.toLowerCase(); 
-
-let arrayAnimes = [];
+let animes = [];
 let favAnimes = [];
 
-function renderAnimes (){
+const handleSearch = () =>{
+    const inputValue = inputFilter.value.toLowerCase();
+    const animesFiltered = animes.filter((anime) =>
+    anime.title.toLowerCase().includes(inputValue));
+    renderAnimes(animesFiltered)
+    console.log(animesFiltered);
+}
+
+inputFilter.addEventListener('keyup', handleSearch)
+
+
+function renderAnimes (array){
     let animeHTML = ''
-    let favAnimeHTML = ''
-    for (const oneAnime of arrayAnimes) {
-        animeHTML += `<li class="anime js_anime" id="${oneAnime.mal_id}">`
+    let favClass = ''
+    const brokenImg = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+    const newImg = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+    for (const oneAnime of array) {
+        const favAnimeFoundIndex = favAnimes.findIndex ((fav) => oneAnime.mal_id === fav.mal_id)
+        if (favAnimeFoundIndex !== -1){
+            favClass = 'anime-fav'
+        }
+        else {
+            favClass =''
+        }
+        animeHTML += `<li class="anime js_anime ${favClass}" id="${oneAnime.mal_id}">`
         animeHTML += `<h2 class="anime__title">${oneAnime.title}</h2>`
-        const brokenImg = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+        
         if(oneAnime.images.jpg.image_url === brokenImg){
-            oneAnime.images.jpg.image_url = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+            oneAnime.images.jpg.image_url = newImg
         }
         animeHTML +=`<img src="${oneAnime.images.jpg.image_url}" alt="Cover of ${oneAnime.title} "> </li>`
 
         
     }
-    searchResult.innerHTML = animeHTML;
+    searchResult.innerHTML = animeHTML; 
+    listenerAnimes();
 }
 
 function listenerAnimes () {
@@ -38,12 +58,12 @@ function listenerAnimes () {
 }
 
 function callApiData () {
-    fetch (`https://api.jikan.moe/v4/anime?q=${inputValue}`)
+    fetch (`https://api.jikan.moe/v4/anime?q=`)
         .then((response) => response.json ())
         .then ((favAnimes) =>{
-            arrayAnimes = favAnimes.data;
-            renderAnimes();
-            listenerAnimes();
+            animes = favAnimes.data;
+            renderAnimes(animes);
+
         }
         )
     }
@@ -52,12 +72,19 @@ function callApiData () {
 function handleClick (ev) {
     ev.preventDefault();
     const animeID = parseInt(ev.currentTarget.id)
-    const animeFound = arrayAnimes.find((anime) => anime.mal_id === animeID)
-    favAnimes.push(animeFound)
+    const animeFound = animes.find((anime) => anime.mal_id === animeID)
+    const favAnimeFound = favAnimes.findIndex((fav) => fav.mal_id === animeID)
 
+    if (favAnimeFound === -1){
+        favAnimes.push(animeFound)
+    }
+    else{
+        favAnimes.splice(favAnimeFound, 1)
+    }
+    renderAnimes(favAnimes)
     console.log(favAnimes);
    // callApiData();
 
 }
 
-// searchBtn.addEventListener('click', handleClick);
+
