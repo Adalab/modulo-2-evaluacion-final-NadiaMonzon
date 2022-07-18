7460cf49c6055638707d806e438093c686d387ac
NadiaMonzon <nadiamonque99@gmail.com>
2022-07-18 08:14:39 +0100

    Remove filter function, add listener and handle click

--- src/js/main.js
+++ src/js/main.js
@@ -6,34 +6,35 @@ const resetBtn = document.querySelector('.js_reset-btn');
 
 const searchResult = document.querySelector('.js_result');
 
+const inputValue = input.value.toLowerCase(); 
+
 let arrayAnimes = [];
 let favAnimes = [];
-
-const inputValue = input.value.toLowerCase(); 
 
 function renderAnimes (){
     let animeHTML = ''
     let favAnimeHTML = ''
     for (const oneAnime of arrayAnimes) {
-        animeHTML += `<li class="anime js_anime"> <h2 class="anime__title">${oneAnime.title}</h2><img src="${oneAnime.images.jpg.image_url}" alt="Cover of ${oneAnime.title} "> </li>`
+        animeHTML += `<li class="anime js_anime" id="${oneAnime.mal_id}">`
+        animeHTML += `<h2 class="anime__title">${oneAnime.title}</h2>`
+        const brokenImg = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+        if(oneAnime.images.jpg.image_url === brokenImg){
+            oneAnime.images.jpg.image_url = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+        }
+        animeHTML +=`<img src="${oneAnime.images.jpg.image_url}" alt="Cover of ${oneAnime.title} "> </li>`
 
-        searchResult.innerHTML = animeHTML;
+        
     }
-    
+    searchResult.innerHTML = animeHTML;
 }
 
- function filterAnimes(){
-    //para filtrar los animes a través del botón de buscar
-    const filteredAnime = arrayAnimes.filter((oneAnime) => oneAnime.title.toLowerCase().includes(inputValue));
-     renderAnimes(filteredAnime);
- }
+function listenerAnimes () {
+    const animesList = document.querySelectorAll('.js_anime')
+    for (const itemList of animesList) {
+        itemList.addEventListener('click', handleClick)
 
-
-function handleClick (ev) {
-    ev.preventDefault();
-    filterAnimes();
-    console.log(arrayAnimes)
-  
+       
+    }
 }
 
 function callApiData () {
@@ -42,10 +43,21 @@ function callApiData () {
         .then ((favAnimes) =>{
             arrayAnimes = favAnimes.data;
             renderAnimes();
+            listenerAnimes();
         }
         )
     }
- callApiData();
+    callApiData();
 
-searchBtn.addEventListener('click', handleClick);
+function handleClick (ev) {
+    ev.preventDefault();
+    const animeID = parseInt(ev.currentTarget.id)
+    const animeFound = arrayAnimes.find((anime) => anime.mal_id === animeID)
+    favAnimes.push(animeFound)
 
+    console.log(favAnimes);
+   // callApiData();
+
+}
+
+// searchBtn.addEventListener('click', handleClick);
